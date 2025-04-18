1538fd33964230fe92f8f71fcaafe9e19e525cd6
Amarjeet Gupta <amar.bst5822@gmail.com>
2025-04-18 23:54:54 +0530

    added with routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import { ActivityPage, ErrorPage, IndexPage, LoginPage, RegisterPage, RepositoriesPage } from "./pages/index.js";
 import App from "./App.jsx";
 
 import { Provider } from "react-redux";
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,15 @@
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage"; 
+import RepositoriesPage from "./RepositoriesPage";
+import ActivityPage from "./ActivityPage";
+import IndexPage from "./IndexPage";
+import ErrorPage from "./ErrorPage";
+
+export {
+  LoginPage,
+  RegisterPage,
+  RepositoriesPage,
+  ActivityPage,
+  IndexPage,
+  ErrorPage
+}
